283fded2f883ce5ed07e3f12d46090105f236687
shrimech <eddinen77@gmail.com>
2026-09-22 04:17:39 +0000

    refactor: clean up commented code in wordpress.sh

--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -4,9 +4,7 @@ set -e
 
 mkdir -p /var/www/html
 
-# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
-#         mv wp-cli.phar /usr/local/bin/wp && \
-# chmod +x /usr/local/bin/wp
+
 
     echo "WordPress core downloaded successfully."
 until mysqladmin ping --host=mariadb --user="${MYSQL_USER}" --password="${MYSQL_PASSWORD}" --silent; do
@@ -26,28 +24,5 @@ if [ ! -f wp-config.php ]; then
 
 fi
     chown --recursive www-data:www-data /var/www/html
-# PHP_FPM_BIN=""
-# if command -v php-fpm >/dev/null 2>&1; then
-#     PHP_FPM_BIN=$(command -v php-fpm)
-# else
-#     for name in php-fpm8.2 php-fpm8 php-fpm7.4 php-fpm; do
-#         if command -v "$name" >/dev/null 2>&1; then
-#             PHP_FPM_BIN=$(command -v "$name")
-#             break
-#         fi
-#     done
-#     for path in /usr/sbin/php-fpm /usr/sbi/2 /usr/sbin/php-fpm8 /usr/sbin/php-fpm7.4; do
-#         if [ -x "$path" ] && [ -z "$PHP_FPM_BIN" ]; then
-#             PHP_FPM_BIN="$path"
-#             break
-#         fi
-#     done
-# fi
 
-# if [ -z "$PHP_FPM_BIN" ]; then
-#     echo "php-fpm binary not found; aborting"
-#     exit 127
-# fi
-
-# echo "Starting $PHP_FPM_BIN"
 exec php-fpm8.2 -F
